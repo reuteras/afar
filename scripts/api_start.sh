b5439f3aaaed68917d72d31b7b97519895822594
Peter Reuterås <peter@reuteras.net>
2018-02-21 07:33:48 +0100

    Add CUCKOO variable and log.

--- scripts/api_start.sh
+++ scripts/api_start.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-# shellcheck disable=SC1091
-. /etc/bash_completion.d/virtualenvwrapper
-workon cuckoo
-cuckoo api > /home/cuckoo/src/cuckoo/log/api.log 2>&1 &
+{
+    # shellcheck disable=SC1091
+    . /etc/bash_completion.d/virtualenvwrapper
+    export CUCKOO=/home/cuckoo/src/cuckoo/.conf
+    workon cuckoo
+    cuckoo api
+} > /home/cuckoo/src/cuckoo/log/api.log 2>&1 &
